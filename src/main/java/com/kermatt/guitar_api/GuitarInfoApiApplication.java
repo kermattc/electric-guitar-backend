1bea7140b30b421d683e4baa42f39c6f9613735a
kermattC <chan.matt624@gmail.com>
2024-12-02 15:08:49 -0500

    created project

---
+++ src/main/java/com/kermatt/guitar_api/GuitarInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kermatt.guitar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarInfoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kermatt/guitar_api/GuitarInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kermatt.guitar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
