abbc0f302ce1c68b64e2103ac4a820f2b53fe356
kermattC <chan.matt624@gmail.com>
2024-12-12 21:04:38 -0500

    Finished styling for now

--- guitar-site/src/app/guitar-cards/guitar-cards.component.ts
+++ guitar-site/src/app/guitar-cards/guitar-cards.component.ts
@@ -3,11 +3,12 @@ import { Guitar } from '../guitar';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-guitar-cards',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatChipsModule],
+  imports: [CommonModule, MatCardModule, MatChipsModule, MatDividerModule],
   templateUrl: './guitar-cards.component.html',
   styleUrls: ['./guitar-cards.component.scss']
 })
